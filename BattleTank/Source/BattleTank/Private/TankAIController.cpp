745fea24a569d63b0f666e29b404ef22f14b4897
tumatihub <jgsoaresdias@gmail.com>
2018-12-31 11:24:10 -0200

    BT139

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Public/TankAIController.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
-#include "Public/TankAIController.h"
+
 
 void ATankAIController::BeginPlay()
 {
