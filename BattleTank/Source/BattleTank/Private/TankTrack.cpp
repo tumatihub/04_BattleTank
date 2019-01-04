cb8fe7f88f8b6a768047eef555c80ad59b2d4bab
tumatihub <jgsoaresdias@gmail.com>
2019-01-04 10:30:47 -0200

    BT162

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,5 +9,11 @@ void UTankTrack::SetThrottle(float Throttle)
 	//auto Time = GetWorld()->GetTimeSeconds();
 	auto TrackName = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s: Throttle: %f"), *TrackName, Throttle)
+
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
